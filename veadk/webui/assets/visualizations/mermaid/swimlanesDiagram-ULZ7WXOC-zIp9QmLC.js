3dcf90267f0df5f59ce84c2c4b4df9a2026606e9
lixuefei.nice <lixuefei.nice@bytedance.com>
2026-09-04 17:56:33 +0800

    fix: align PR review codex session protocol

---
+++ veadk/webui/assets/visualizations/mermaid/swimlanesDiagram-ULZ7WXOC-zIp9QmLC.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-BwkBXtjN.js";import{a}from"./mermaid.core-BT7iH2aE.js";import"../../app/index-BFPSpx4a.js";import"./chunk-5VM5RSS4-C6gyMZR5.js";import"./chunk-XXDRQBXY-DI0Jtng3.js";import"./chunk-KBJHAD2P-D_mMDCzv.js";import"./chunk-2GRJ4B5K-BsBAvwzC.js";import"../../chunks/channel-BjTsvj70.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
